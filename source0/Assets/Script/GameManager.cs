2363905bd18a22851fc16c790c97bcf4f191b428
emptypizzaa <linelinewith@gmail.com>
2024-07-17 21:19:59 +0900

    vvvvinacademygreen
    vvvvinacademygreen

--- source0/Assets/Script/GameManager.cs
+++ source0/Assets/Script/GameManager.cs
@@ -5,12 +5,16 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //pc1 .cs 조작
+        //pc2(ai).cs
+        //ui 모바일 조작 선택지 실시간
+        //gamelogic states
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // -100 0 100
+        //choco cake 축
     }
 }
